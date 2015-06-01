1f9e9e12887a3474b17194e3799de71023d7782d
Nathan Stilwell <nstilwell@gilt.com>
2015-06-01 16:07:55 -0400

    :metal: Hyping up the Less Gulp Task
    Rolling out the welcome mat for our new friends with luxurious comforts like
    sourcemaps and less plugins. If you MUST use Bootstrap, then lets bring it in
    with the official npm plugin rather than crawling directories like a philistine.

--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -1,37 +1,33 @@
+/*global require  */
+
+'use strict';
+
+/**
+ * Less Task for gulp
+ *
+ * This task takes all less files from the config file and compiles them to CSS.
+ * We are including source maps in the compiled css using the gulp-sourcemaps
+ * module, and are relying on a less plugin to include less files from npm
+ * modules (bootstrap).
+ */
+
 var gulp  = require('gulp');
 var less  = require('gulp-less');
 var debug = require('gulp-debug');
+var sourcemaps = require('gulp-sourcemaps');
+var LessPluginNpmImport = require('less-plugin-npm-import');
+var npmimport = new LessPluginNpmImport();
 
 var LESS_BASE_FILE = require('../config').css.LESS_BASE_FILE;
 var DEST_FOLDER = require('../config').css.CSS_DEST_FOLDER;
-var GH_PAGES_CSS_FILE = require('../config').css.GH_PAGES_CSS_FILE;
 
-
-/**
- * Gulp task to convert less to css for the module
- *
-*/
-gulp.task('less-module', function() {
+gulp.task('less', function() {
   return gulp.src(LESS_BASE_FILE)
+          .pipe(sourcemaps.init())
           .pipe(debug({title : 'debug-less-module'}))
-          .pipe(less())
+          .pipe(less({
+            plugins: [npmimport]
+          }))
+          .pipe(sourcemaps.write())
           .pipe(gulp.dest(DEST_FOLDER));
 });
-
-
-/**
- * Gulp task to convert less to css for the github page
- *
-*/
-gulp.task('less-gh-pages', function() {
-  return gulp.src(GH_PAGES_CSS_FILE)
-          .pipe(debug({title : 'debug-less-gh-pages'}))
-          .pipe(less())
-          .pipe(gulp.dest(DEST_FOLDER));
-});
-
-/**
- * Gulp task to convert less to css for both module and github page
- *
-*/
-gulp.task('less', ['less-module', 'less-gh-pages']);
