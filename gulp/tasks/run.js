d1b0266a9c488269c00dcd80e5275c8a1a31e3b7
Nathan Stilwell <nstilwell@gilt.com>
2015-06-01 16:04:33 -0400

    :fire: Don't need this anymore

--- gulp/tasks/run.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var beefy = require('beefy');
-var http = require('http');
-var handler = beefy(require('../config').js.JS_DEST_BROWSERIFIED_FILE);
-
-gulp.task('run', function() {
-  return http.createServer(handler).listen(8000);
-});
